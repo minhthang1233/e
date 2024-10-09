22c7c46e50ba91a32e2fc93b9d101d475f49ce73
minhthang1233 <nkoccoi321@gmail.com>
2024-10-09 21:35:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)  # Lắng nghe trên cổng Render
+    app.run(host='0.0.0.0', port=port, debug=True)  # Bật chế độ debug
